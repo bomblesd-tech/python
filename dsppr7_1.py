9670f1e46d4b51e3be2e18c7506e12b46c6b8cc5
bomblesd-tech <bomblesd@jdcoem.ac.in>
2026-09-08 16:52:59 +0530

    Add files via upload

---
+++ dsppr7_1.py
@@ -0,0 +1,17 @@
+def linear_search(arr, key):
+    i = 0
+    while i < len(arr):
+        if arr[i] == key:
+            return i
+        i = i + 1
+    return -1
+
+arr = [10, 25, 30, 45, 60]
+key = 30
+
+result = linear_search(arr, key)
+
+if result != -1:
+    print("Element found at index:", result)
+else:
+    print("Element not found")
